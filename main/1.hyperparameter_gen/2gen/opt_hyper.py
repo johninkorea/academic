9fbf2bdd494c43135f121af84494e872bffd6e24
johninkorea <john829829@gmail.com>
2022-11-08 02:33:27 +0900

    asd

--- main/1.hyperparameter_gen/2gen/opt_hyper.py
+++ main/1.hyperparameter_gen/2gen/opt_hyper.py
@@ -15,7 +15,7 @@ hyper=np.array([1, 2, 3, 4],dtype=float)
 
 def fitness_func(solution, solution_idx):
 
-    abs_error = nnn(solution)*1000 + 0.00000001
+    abs_error = nnn(solution) + 0.00000001
 
     solution_fitness = 1.0 / abs_error
 
@@ -27,27 +27,33 @@ last_fitness = 0
 def callback_generation(ga_instance):
     global last_fitness
     print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print(ga_instance.population)
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
+    # print(ga_instance.population)
+    # print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+    # print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
     last_fitness = ga_instance.best_solution()[1]
+    # print("*"*30)
 
-ga_instance = pygad.GA(num_generations=100,
-                       num_parents_mating=5,
+ga_instance = pygad.GA(num_generations=1000,
+                       sol_per_pop=100,
+                       num_parents_mating=50,
+                       mutation_type=None,
+                       parent_selection_type = "sss",
                        fitness_func=fitness_function,
-                       sol_per_pop=10,
                        num_genes=len(hyper),
                        on_generation=callback_generation,
                        gene_space=[{'low': 1e-3, 'high': 10}, {'low': 1, 'high': 10}, {'low': 1, 'high': 10}, {'low': 1, 'high': 10}],
-                       gene_type=[float, int, int, int],
-                       mutation_type=None)
+                       gene_type=[float, int, int, int])
 
 
 # print(ga_instance.population)
 ga_instance.run()
 
+ga_instance.plot_fitness() 
 # plot result
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 print("Parameters of the best solution : {solution}".format(solution=solution))
 print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+
+if ga_instance.best_solution_generation != -1:
+    print("Best fitness value reached after {best_solution_generation} generations.".format(best_solution_generation=ga_instance.best_solution_generation))
