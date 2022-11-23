36ca99a10ef76bb036c243bb3d28bdf54da95e51
johninkorea <john829829@gmail.com>
2022-11-24 02:46:02 +0900

    asd

--- 3.Final/pinn.py
+++ 3.Final/pinn.py
@@ -113,7 +113,7 @@ def pinn(hyper, generations, gif=False):
         
         # compute the "data loss"
         yh = model(x_data.to(device))
-        loss1 = torch.nn.MSELoss(yh-y_data).to(device)# use mean squared error
+        loss1 = torch.nn.MSELoss(yh,y_data).to(device)# use mean squared error
         # loss1 = torch.mean((yh-y_data)**2)# use mean squared error
         
         # compute the "physics loss"
