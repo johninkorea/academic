28be4cb161590c998569013dd5547b48bbc5fbc3
johninkorea <john829829@gmail.com>
2022-11-25 17:27:51 +0900

    asd

--- 3.Final/opt_pinn.py
+++ 3.Final/opt_pinn.py
@@ -7,12 +7,9 @@ import time as T
 seed=1114
 np.random.seed(seed)
 
-# hyper=np.array([1e-4,15000,32,3])
 # learingrate, number_of_epoch, nodes_per_hidden, number_of_hidden
-# asd=pinn(hyper=hyper, gif=False)
-# print(asd)
 learingrate_min, learingrate_max = 1e-4, 1e-1
-number_of_epoch_min, number_of_epoch_max = 1, int(1e4)
+number_of_epoch_min, number_of_epoch_max = 1, int(9e3)
 nodes_per_hidden_min, nodes_per_hidden_max = 1, 20
 number_of_hidden_min, number_of_hidden_max = 1, 20
 
@@ -28,12 +25,16 @@ def fitness_func(solution, solution_idx):
 last_fitness = 0
 def callback_generation(ga_instance):
     global last_fitness
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
     print(f"Generation = {ga_instance.generations_completed}"+"*********************************")
     print(ga_instance.population) # print population
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
+    print(f"Fitness    = {solution_fitness}")
+    print(f"Change     = {solution_fitness - last_fitness}")
+    print(f"Parameters of the best solution : {solution}")
+    print(f"Index of the best solution : {solution_idx}")
+    if ga_instance.best_solution_generation != -1:
+        print(f"Best fitness value reached after {ga_instance.best_solution_generation} generations.")
     last_fitness = ga_instance.best_solution()[1]
-    # print("*"*30)
 
 ## Conditions
 # population
@@ -99,9 +100,12 @@ f=T.time()
 print("time: ",f-s)
 # ga_instance.plot_fitness() 
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
-print("Parameters of the best solution : {solution}".format(solution=solution))
-print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
-print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
-
+print(f"Parameters of the best solution : {solution}")
+print(f"Fitness value of the best solution = {solution_fitness}")
+print(f"Index of the best solution : {solution_idx}")
 if ga_instance.best_solution_generation != -1:
-    print("Best fitness value reached after {best_solution_generation} generations.".format(best_solution_generation=ga_instance.best_solution_generation))
+    print(f"Best fitness value reached after {ga_instance.best_solution_generation} generations.")
+
+
+loss=(1/solution_fitness)-0.00000001
+print("Loss of best pinn : ", loss)
