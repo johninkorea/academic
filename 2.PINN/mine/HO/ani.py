e15fb563e15cc8ec232edb874974aa010219ffbf
johninkorea <john829829@gmail.com>
2022-11-26 19:07:11 +0900

    asd

--- 2.PINN/mine/HO/ani.py
+++ 2.PINN/mine/HO/ani.py
@@ -8,14 +8,16 @@ def save_gif_PIL(outfile, files, fps=5, loop=0):
     imgs[0].save(fp=outfile, format='GIF', append_images=imgs[1:], save_all=True, duration=int(1000/fps), loop=loop)
 
 files = []
-
 for i in range(15000):
     # plot the result as training progresses
     if (i+1) % 10 == 0: 
         
         file = f"plots/nn_{i+1}.png"
         files.append(file)
-            
+z=0
+while z<1000:
+    files.append(files[-1])
+    z+=1
 save_gif_PIL("result/nn.gif", files, fps=20, loop=0)
 
 files = []
@@ -25,6 +27,10 @@ for i in range(15000):
 
         file = "plots/pinn_%.8i.png"%(i+1)
         files.append(file)
+z=0
+while z<1000:
+    files.append(files[-1])
+    z+=1
 save_gif_PIL("result/pinn.gif", files, fps=20, loop=0)
 
 
